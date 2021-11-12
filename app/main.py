164c1089a87f8ed7cca1041baae3ab06744904de
swap-stack <swapnilakavishwar@gmail.com>
2021-11-12 10:53:01 +0530

    Updated endpoints and error handling

--- app/main.py
+++ app/main.py
@@ -5,49 +5,74 @@ import subprocess
 import json
 import os
 
+data_file_path = "../data/game_ids.csv"
+output_file_path = "../data/output.json"
+
 app = FastAPI()
+
 
 class GameItem(BaseModel):
     name: str
     id: str
     price: str
 
+
 class GameItemList(BaseModel):
     data: List[GameItem]
 
-@app.get("/all_prices")
+
+@app.get("/items_prices")
 async def get_all_game_items():
-    if os.path.exists("../data/output.json"):
-        os.remove("../data/output.json")
-    with open("../data/game_ids.csv", 'r') as f:
-        ids = f.read()
-    if ids:
-        game_ids = str(ids)
-        cmd = f'scrapy crawl game -a game_ids={game_ids} -o output.json'
-        subprocess.check_output(cmd)
-        data = None
-        with open('../data/output.json') as file:
-            data = json.load(file)
+    data = None
+
+    if os.path.exists(output_file_path):
+        os.remove(output_file_path)
+    try:
+        with open(data_file_path, 'r') as f:
+            ids = f.read()
+        if ids:
+            game_ids = str(ids)
+            cmd = f'scrapy crawl game -a game_ids={game_ids} -o output.json'
+            subprocess.check_output(cmd)
+            with open(output_file_path) as file:
+                data = json.load(file)
+    except:
+        # TODO
+        print('Error')
+        # return appropriate response
     return {'message': data}
 
-@app.post("/add", status_code=201)
+
+@app.post("/game", status_code=201)
 async def add_an_item(game_id):
-    with open("../data/game_ids.csv", "a") as f:
-        f.write(f",{game_id}")
+    try:
+        with open(data_file_path, "a") as f:
+            f.write(f",{game_id}")
+        return {"message": "OK"}
+    except:
+        # TODO
+        print('Error')
+        # return appropriate response
+        return {"message": "Error"}
 
-    return {"message": "OK"}
 
-@app.delete("/delete", status_code=204)
+@app.delete("/game", status_code=204)
 async def delete_an_item(game_id):
-    with open("../data/game_ids.csv", "r") as f:
-        ids = f.read()
-        ids = ids.split(',')
-        if game_id not in ids:
-            return {"message": "Could not locate"}
-        ids.remove(game_id)
-    updated_ids = ','.join(ids)
+    try:
+        with open(data_file_path, "r") as f:
+            ids = f.read()
+            ids = ids.split(',')
+            if game_id not in ids:
+                return {"message": "Could not locate"}
+            ids.remove(game_id)
+        updated_ids = ','.join(ids)
 
-    with open("../data/game_ids.csv", "w") as f:
-        f.write(updated_ids)
+        with open(data_file_path, "w") as f:
+            f.write(updated_ids)
+        return {"message": "OK"}
 
-    return {"message": "OK"}
+    except:
+        # TODO
+        print('Error')
+        # return appropriate response
+        return {"message": "Error"}
